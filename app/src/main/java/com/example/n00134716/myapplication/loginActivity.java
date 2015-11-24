f2776955af03ee63d714d4b1e781a341e801ece9
gio <giovannifusciardi@hotmail.com>
2015-11-24 14:07:53 +0000

    Initial commit

---
+++ app/src/main/java/com/example/n00134716/myapplication/loginActivity.java
@@ -0,0 +1,53 @@
+package com.example.n00134716.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class loginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void onLogin (View v) {
+
+        Toast.makeText(loginActivity.this, R.string.toast_login_pressed, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void onRegister (View v) {
+
+        Toast.makeText(loginActivity.this, R.string.toast_Register_pressed, Toast.LENGTH_SHORT).show();
+
+    }
+}
